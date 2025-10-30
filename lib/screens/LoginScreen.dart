9a0a26024cf8a813e9cb70537dfeb747f02bfda8
DiegoJimenez <die.jimenezc@duocuc.cl>
2025-10-29 23:50:32 -0300

    Commit inicio rama diego
    inicia las implementaciones de la rama

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../screens/AdminScreen.dart';
-import '../screens/AdminScreen.dart';
 import '../screens/CocinaScreen.dart';
 import '../screens/MeseroScreen.dart';
 
